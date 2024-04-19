ab261b766a8f1153baece9ba1dfa8839abbd6d4c
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-19 15:21:01 +0200

    DOM list

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 let i;
+const ul = document.querySelector("ul");
 
 for (i = 1; i <= 100; i++) {
+	const li = document.createElement("li");
 	if (i % 3 == 0 && i % 5 == 0) {
-		console.log("FizzBuzz");
+		li.append(`FizzBuzz`);
+		ul.append(li);
 	} else if (i % 3 == 0) {
-		console.log("Fizz");
+		li.append(`Fizz`);
+		ul.append(li);
 	} else if (i % 5 == 0) {
-		console.log("Buzz");
+		li.append(`Buzz`);
+		ul.append(li);
 	} else {
-		console.log(i);
+		li.append(i);
+		ul.append(li);
 	}
 }
